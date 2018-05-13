3c02039f9528aa5a3390a49e089de12aef9a4073
John <John@192.168.0.111>
2018-05-12 17:26:05 -0700

    line breaks

--- AdventureGame/src/Game/Setup.java
+++ AdventureGame/src/Game/Setup.java
@@ -5,7 +5,7 @@ public class Setup {
 	public static Room Initiatlize() {		
 		Room Master = new Room("Master Bedroom","You are in a fairly large room with a queen-size bed");
 		Room Hallway = new Room("South end of Hallway","You are in a hallway with a number of doors");
-		Room Closet = new Room("Closet","This is the closet in the Master Bedroom.\nThere's nobody living in here at the moment");
+		Room Closet = new Room("Closet","This is the closet in the Master Bedroom.\r\nThere's nobody living in here at the moment");
 		Master.addLink(new Link(Master,Closet,"east"));
 		Master.addItem(new Item("Pillow","It's so soft and light it almost floats away"));
 		Closet.addLink(new Link(Closet,Master,"west"));
@@ -18,10 +18,10 @@ public class Setup {
 		Room HallEnd = new Room("North end of Hallway","You are in a hallway with a number of doors");
 		Hallway.addLink(new Link(Hallway,HallEnd,"north"));
 		HallEnd.addLink(new Link(HallEnd,Hallway,"south"));
-		Room Bedroom2 = new Room("Bedroom 2","This is a girl's room\nThere is a small canopy bed beneath a pile of stuffed animals");
+		Room Bedroom2 = new Room("Bedroom 2","This is a girl's room\r\nThere is a small canopy bed beneath a pile of stuffed animals");
 		Bedroom2.addLink(new Link(Bedroom2,Hallway,"east"));
 		Hallway.addLink(new Link(Hallway,Bedroom2,"west"));
-		Room Bedroom3 = new Room("Bedroom 3","This is a teenage boy's bedroom\nThere is a twin-size water bed against one wall");
+		Room Bedroom3 = new Room("Bedroom 3","This is a teenage boy's bedroom\r\nThere is a twin-size water bed against one wall");
 		Bedroom3.addLink(new Link(Bedroom3,HallEnd,"east"));
 		HallEnd.addLink(new Link(HallEnd,Bedroom3,"west"));
 		
